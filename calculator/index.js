e4af01d09c90fc6a250d0e51a7d7ab16f405c572
fares takorabt <farestakorabt@gmail.com>
2022-01-05 10:20:28 +0100

    end of the calculator

--- calculator/index.js
+++ calculator/index.js
@@ -1,1 +1,16 @@
+const buttons = document.querySelectorAll(".btn");
+const result = document.querySelector("h3");
 
+buttons.forEach((buttons) => {
+  buttons.addEventListener("click", (e) => {
+    result.textContent += e.target.id;
+  });
+});
+
+equal.addEventListener("click", () => {
+  result.textContent = eval(result.textContent);
+});
+
+clear.addEventListener("click", () => {
+  result.textContent = "";
+});
